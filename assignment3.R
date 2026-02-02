cf4a5e4a6fa493f2b54c45e8f80733788f83cef3
swagyes <90705669+swagyes@users.noreply.github.com>
2026-02-02 02:41:33 -0500

    Create assignment3.R

---
+++ assignment3.R
@@ -0,0 +1,10 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS <- c(12, 75, 43, 19, 1, 21, 19)
+
+poll_data <- data.frame(Name, ABC, CBS)
+
+
+poll_data
