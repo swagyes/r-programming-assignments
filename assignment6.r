31ae9973d3b8bce7d2cce6feb3099760296370fa
swagyes <90705669+swagyes@users.noreply.github.com>
2026-02-23 01:27:11 -0500

    Create assignment6.r

---
+++ assignment6.r
@@ -0,0 +1,15 @@
+#question 1
+A <- matrix(c(2,0,1,3), ncol = 2)
+B <- matrix(c(5,2,4,-1), ncol = 2)
+
+A + B
+A - B
+
+#question 2
+diag(c(4,1,2,3))
+
+#question 3
+M <- diag(3, 5)
+M[1, ] <- c(3,1,1,1,1)
+M[,1] <- c(3,2,2,2,2)
+M
