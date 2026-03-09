2f77a6067c4549363739eb2c1125d83078d4c793
swagyes <90705669+swagyes@users.noreply.github.com>
2026-03-09 00:38:31 -0400

    Update assignment8.R

--- assignment8.R
+++ assignment8.R
@@ -12,3 +12,5 @@ write.table(StudentAverage, "Students_Gendered_Mean", sep = ",")
 i_students <- subset(Student, grepl("[iI]", Student$Name))
 i_students
 write.table(i_students, "DataSubset", sep = ",")
+
+#https://rprogramingorestes.blogspot.com/2026/03/module-8-inputoutput-string.html
